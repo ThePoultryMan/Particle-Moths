a8178769400c86a49da0905464d3fce389881c20
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-05-04 16:07:24 -0400

    Initial mod set up

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# Mod Template
+# Particle Moths
 
-## About
-
-This is a mod template created by ThePoultryMan on GitHub. It was forked from the [fabric-example-mod](https://github.com/FabricMC/fabric-example-mod).
-The mod has switched to Quilt Mappings and I have changed the format to suit my needs of creating a new mod.
+Adds moths to Minecraft using particles.
--- src/main/java/io/github/thepoultryman/particlemoths/ParticleMoths.java
+++ src/main/java/io/github/thepoultryman/particlemoths/ParticleMoths.java
@@ -1,15 +1,15 @@
-package thepoultryman.modid;
+package io.github.thepoultryman.particlemoths;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ModClass implements ModInitializer {
-	public static final String MOD_ID = "modid";
+public class ParticleMoths implements ModInitializer {
+	public static final String MOD_ID = "particlemoths";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Initializing [inset mod name here]");
+		LOGGER.info("Initializing (Particle) Moths");
 	}
 }
