362045df749e1ad6159f14c09e67d57a05c1482d
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-05-04 17:00:29 -0400

    Basic implementation

---
+++ src/main/java/io/github/thepoultryman/particlemoths/MothParticle.java
@@ -0,0 +1,33 @@
+package io.github.thepoultryman.particlemoths;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.particle.*;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.particle.DefaultParticleType;
+
+public class MothParticle extends AbstractSlowingParticle {
+    protected MothParticle(ClientWorld clientWorld, double d, double e, double f, double g, double h, double i) {
+        super(clientWorld, d, e, f, g, h, i);
+    }
+
+    @Override
+    public ParticleTextureSheet getType() {
+        return ParticleTextureSheet.PARTICLE_SHEET_OPAQUE;
+    }
+
+    @Environment(EnvType.CLIENT)
+    public static class Factory implements ParticleFactory<DefaultParticleType> {
+        private final SpriteProvider spriteProvider;
+
+        public Factory(SpriteProvider spriteProvider) {
+            this.spriteProvider = spriteProvider;
+        }
+
+        public Particle createParticle(DefaultParticleType defaultParticleType, ClientWorld clientWorld, double d, double e, double f, double g, double h, double i) {
+            MothParticle mothParticle = new MothParticle(clientWorld, d, e, f, g, h, i);
+            mothParticle.setSprite(this.spriteProvider);
+            return mothParticle;
+        }
+    }
+}
--- src/main/java/io/github/thepoultryman/particlemoths/ParticleMoths.java
+++ src/main/java/io/github/thepoultryman/particlemoths/ParticleMoths.java
@@ -1,6 +1,15 @@
 package io.github.thepoultryman.particlemoths;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.particle.ParticleEffect;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,8 +17,25 @@ public class ParticleMoths implements ModInitializer {
 	public static final String MOD_ID = "particlemoths";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static final DefaultParticleType MOTH = FabricParticleTypes.simple();
+
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Initializing (Particle) Moths");
+
+		Registry.register(Registry.PARTICLE_TYPE, new Identifier(MOD_ID, "moth"), MOTH);
+
+		ClientTickEvents.END_CLIENT_TICK.register(this::createMothParticle);
+	}
+
+	private void createMothParticle(MinecraftClient client) {
+		if (client.world != null) {
+			World world = client.world;
+			PlayerEntity player = client.player;
+
+			double velocity = world.getRandom().nextDouble(-0.5D, 0.5D);
+
+			world.addParticle((ParticleEffect) Registry.PARTICLE_TYPE.get(new Identifier("particlemoths:moth")), player.getX() + 3D, player.getY(), player.getZ() + 3D, velocity, velocity, velocity);
+		}
 	}
 }
