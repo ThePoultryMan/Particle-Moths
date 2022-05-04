541dfacad8c3df422faf9d3cac3813ebe9ece3d9
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-05-04 18:00:20 -0400

    Remove unnecessary code

--- src/main/java/io/github/thepoultryman/particlemoths/ParticleMothsClient.java
+++ src/main/java/io/github/thepoultryman/particlemoths/ParticleMothsClient.java
@@ -2,17 +2,10 @@ package io.github.thepoultryman.particlemoths;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
-import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
-import net.minecraft.screen.PlayerScreenHandler;
-import net.minecraft.util.Identifier;
 
 public class ParticleMothsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        ClientSpriteRegistryCallback.event(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE).register(((atlasTexture, registry) -> {
-            registry.register(new Identifier(ParticleMoths.MOD_ID, "particle/moth"));
-        }));
-
         ParticleFactoryRegistry.getInstance().register(ParticleMoths.MOTH, MothParticle.Factory::new);
     }
 }
