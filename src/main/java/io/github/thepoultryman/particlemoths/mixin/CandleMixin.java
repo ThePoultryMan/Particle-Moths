22c85c650a364d235fe1a88907a0cad9d0b25095
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-05-06 19:15:49 -0400

    Add Candle mixin

---
+++ src/main/java/io/github/thepoultryman/particlemoths/mixin/CandleMixin.java
@@ -0,0 +1,27 @@
+package io.github.thepoultryman.particlemoths.mixin;
+
+import io.github.thepoultryman.particlemoths.MothSpawnHelper;
+import net.minecraft.block.AbstractCandleBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
+
+@Mixin(AbstractCandleBlock.class)
+public class CandleMixin {
+    @Shadow @Final public static BooleanProperty LIT;
+
+    @Inject(at = @At("TAIL"), method = "randomDisplayTick")
+    private void particlemoths$spawnMothByCandle(BlockState state, World world, BlockPos pos, Random random, CallbackInfo ci) {
+        if (state.get(LIT))
+            MothSpawnHelper.spawnMothByBlock(world, pos);
+    }
+}
