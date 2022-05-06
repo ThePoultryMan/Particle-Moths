97b44b5207aed4fae933a7b90bce2ff478291707
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-05-05 21:34:54 -0400

    Initial redstone lamp mixin

---
+++ src/main/java/io/github/thepoultryman/particlemoths/mixin/RedstoneLampMixin.java
@@ -0,0 +1,22 @@
+package io.github.thepoultryman.particlemoths.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.RedstoneLampBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+
+import java.util.Random;
+
+@Mixin(RedstoneLampBlock.class)
+public abstract class RedstoneLampMixin extends Block {
+    public RedstoneLampMixin(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public void randomDisplayTick(BlockState state, World world, BlockPos pos, Random random) {
+        super.randomDisplayTick(state, world, pos, random);
+    }
+}
