d8b5ac3d1ffd71a3c9a666b86738aa52f0a89095
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-07-17 18:46:02 -0400

    Initial config screen

--- src/main/java/io/github/thepoultryman/particlemoths/config/ModMenuIntegration.java
+++ src/main/java/io/github/thepoultryman/particlemoths/config/ModMenuIntegration.java
@@ -1,13 +1,13 @@
-/*
 package io.github.thepoultryman.particlemoths.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import io.github.thepoultryman.particlemoths.config.screen.ConfigScreen;
+import net.minecraft.client.gui.screen.Screen;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-
+        return (ConfigScreenFactory<Screen>) ConfigScreen::new;
     }
 }
-*/
