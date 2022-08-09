2e49765411b28112d50e9e28324611a34920c58f
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-08-09 16:20:37 -0400

    Remove ScreenCustomizer

--- src/main/java/io/github/thepoultryman/particlemoths/config/ModMenuIntegration.java
+++ src/main/java/io/github/thepoultryman/particlemoths/config/ModMenuIntegration.java
@@ -3,18 +3,14 @@ package io.github.thepoultryman.particlemoths.config;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import io.github.thepoultryman.cactusconfig.screen.ConfigScreen;
-import io.github.thepoultryman.cactusconfig.screen.ScreenCustomizer;
 import io.github.thepoultryman.particlemoths.ParticleMoths;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.TranslatableText;
 
 public class ModMenuIntegration implements ModMenuApi {
-    private final ScreenCustomizer screenCustomizer = new ScreenCustomizer(new TranslatableText("config.particlemoths.title"));
-
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        screenCustomizer.useOneOptionColumn();
-        return (ConfigScreenFactory<Screen>) parent -> new ConfigScreen(screenCustomizer, parent, ParticleMoths.CONFIG,
+        return (ConfigScreenFactory<Screen>) parent -> new ConfigScreen(new TranslatableText("config.particlemoths.title"), parent, ParticleMoths.CONFIG,
                 ParticleMoths.CONFIG.generalTab, ParticleMoths.CONFIG.spawningTab, ParticleMoths.CONFIG.blockSpawningTab);
     }
 }
