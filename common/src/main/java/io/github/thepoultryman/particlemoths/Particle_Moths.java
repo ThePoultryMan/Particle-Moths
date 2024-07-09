b255f1febe61d7476af5666186f905d1ef3b0528
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-07-08 20:46:41 -0400

    Revert back to MidnightLib

---
+++ common/src/main/java/io/github/thepoultryman/particlemoths/Particle_Moths.java
@@ -0,0 +1,9 @@
+package io.github.thepoultryman.particlemoths;
+
+public class Particle_Moths {
+    public static final String MOD_ID = "particlemoths";
+
+    public static void init() {
+
+    }
+}
