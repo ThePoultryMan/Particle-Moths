63aecdb10060966cc2d8934f09649f0f23482048
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2024-07-12 00:39:58 -0400

    refactor(port): ambient moth spawning

--- fabric/src/main/java/io/github/thepoultryman/particlemoths/fabric/ParticleMothsFabric.java
+++ fabric/src/main/java/io/github/thepoultryman/particlemoths/fabric/ParticleMothsFabric.java
@@ -1,10 +1,16 @@
 package io.github.thepoultryman.particlemoths.fabric;
 
 import io.github.thepoultryman.particlemoths.MothParticle;
+import io.github.thepoultryman.particlemoths.MothSpawnHelper;
 import io.github.thepoultryman.particlemoths.ParticleMoths;
 import io.github.thepoultryman.particlemoths.mixins.SimpleParticleTypeInvoker;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Registry;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.core.registries.BuiltInRegistries;
@@ -19,5 +25,23 @@ public class ParticleMothsFabric implements ClientModInitializer {
 
         Registry.register(BuiltInRegistries.PARTICLE_TYPE, ResourceLocation.fromNamespaceAndPath(ParticleMoths.MOD_ID, "moth"), MOTH);
         ParticleFactoryRegistry.getInstance().register(ParticleMoths.getOptions(), MothParticle.Factory::new);
+
+        ClientTickEvents.END_CLIENT_TICK.register(this::createMothParticle);
+    }
+
+    private void createMothParticle(Minecraft minecraft) {
+        if (minecraft.level == null || !MothSpawnHelper.shouldSpawnMoth(minecraft)) return;
+
+        ClientLevel level = minecraft.level;
+        LocalPlayer player = minecraft.player;
+
+        if (player == null) return;
+
+        BlockPos spawnPos = MothSpawnHelper.getSpawnCoordinates(player.getOnPos());
+        if (!MothSpawnHelper.isValidMothSpawn(level, spawnPos, minecraft)) return;
+        double[] velocities = MothSpawnHelper.getVelocity();
+
+        level.addParticle(ParticleMoths.getOptions(),
+                spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), velocities[0], velocities[1], velocities[2]);
     }
 }
--- neoforge/src/main/java/io/github/thepoultryman/particlemoths/neoforge/ParticleMothsNeoForge.java
+++ neoforge/src/main/java/io/github/thepoultryman/particlemoths/neoforge/ParticleMothsNeoForge.java
@@ -1,12 +1,19 @@
 package io.github.thepoultryman.particlemoths.neoforge;
 
 import io.github.thepoultryman.particlemoths.MothParticle;
+import io.github.thepoultryman.particlemoths.MothSpawnHelper;
 import io.github.thepoultryman.particlemoths.ParticleMoths;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.RegisterParticleProvidersEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -20,8 +27,30 @@ public class ParticleMothsNeoForge {
         ParticleMoths.init();
     }
 
-    @SubscribeEvent
-    public static void registerParticleProviders(RegisterParticleProvidersEvent event) {
-        event.registerSpriteSet(ParticleMoths.getOptions(), MothParticle.Factory::new);
+    @EventBusSubscriber(modid = ParticleMoths.MOD_ID)
+    private static class EventHandler {
+        @SubscribeEvent
+        public static void registerParticleProviders(RegisterParticleProvidersEvent event) {
+            event.registerSpriteSet(ParticleMoths.getOptions(), MothParticle.Factory::new);
+        }
+
+        @SubscribeEvent
+        public static void clientPostTick(ClientTickEvent.Post event) {
+            var minecraft = Minecraft.getInstance();
+
+            if (minecraft.level == null || !MothSpawnHelper.shouldSpawnMoth(minecraft)) return;
+
+            ClientLevel level = minecraft.level;
+            LocalPlayer player = minecraft.player;
+
+            if (player == null) return;
+
+            BlockPos spawnPos = MothSpawnHelper.getSpawnCoordinates(player.getOnPos());
+            if (!MothSpawnHelper.isValidMothSpawn(level, spawnPos, minecraft)) return;
+            double[] velocities = MothSpawnHelper.getVelocity();
+
+            level.addParticle(ParticleMoths.getOptions(),
+                    spawnPos.getX(), spawnPos.getY(), spawnPos.getZ(), velocities[0], velocities[1], velocities[2]);
+        }
     }
 }
